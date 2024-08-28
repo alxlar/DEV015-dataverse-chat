22c19eb4422557493442cb7fcd25c5fa4532b5ff
Alexandra Alarcon <alealarcon067@gmail.com>
2024-08-27 20:01:04 -0500

    prueba en index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { setRootEl, setRoutes, onURLChange } from './routes.js';
-import Home  from './views/HomeView.js';
-import Details  from './views/DetailView.js';
-import ApikeyView from './views/ApikeyView.js'
-import Error from "./views/Error.js";
-import ChatView from "./views/ChatView.js";
-import ChatGroup from "./views/ChatGroup.js";
+import Home  from '/views/HomeView.js';
+import Details  from '/views/DetailView.js';
+import ApikeyView from '/views/ApikeyView.js'
+import Error from "/views/Error.js";
+import ChatView from "/views/ChatView.js";
+import ChatGroup from "/views/ChatGroup.js";
 
 const routes = {
   '/': Home,
